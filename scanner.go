8d6b82f8ac1938650ab6f0c9000a073e9477692f
Gabe Conradi <gabe@tumblr.com>
2015-05-06 18:04:05 -0400

    more dreck

--- scanner.go
+++ scanner.go
@@ -1,59 +1,63 @@
 package main
 
 import (
-  "fmt"
-  "net"
-  "os/exec"
-  "log"
-  "bufio"
+	"bytes"
+	"fmt"
+	"log"
+	"net"
+	"os/exec"
+	"strings"
 )
 
 type SshPublicKey struct {
-  Hostname  string
-  Ips       []net.IP
-  Aliases   []string
-  PublicKey string
+	Hostname  string
+	Ips       []net.IP
+	Aliases   []string
+	PublicKey string
 }
 
 func ScanHost(host string, timeout int) (SshPublicKey, error) {
-  s := SshPublicKey{Hostname: host}
-  // first, resolve the host to IP[s]. Some VIPs may map to multiple backends, so track them all
-  ips, err := net.LookupIP(host)
-  if err != nil {
-    return s, fmt.Errorf("Unable to resolve %s to IP: %s", host, err)
-  }
-  s.Ips = ips
-  // ssh-keyscan -t rsa,dsa,ecdsa $(hostname -f),172.18.110.119,fuckface
-  // append comment to end
-  return s, nil
+	s := SshPublicKey{Hostname: host}
+	// first, resolve the host to IP[s]. Some VIPs may map to multiple backends, so track them all
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		return s, fmt.Errorf("Unable to resolve %s to IP: %s", host, err)
+	}
+	s.Ips = ips
+	// ssh-keyscan -t rsa,dsa,ecdsa $(hostname -f),172.18.110.119,fuckface
+	aliases := s.AliasList()
+	aliaslist := strings.Join(aliases, ",")
+	scan_output, err := RunCommand(fmt.Sprintf("ssh-keyscan -t rsa %s", aliaslist))
+	if err != nil {
+		return s, fmt.Errorf("Unable to scan %s: %s", s.Hostname, err)
+	}
+	//TODO format the scan_output properly, handle failures, etc
+	s.PublicKey = scan_output
+	return s, nil
+}
+
+func (s *SshPublicKey) AliasList() []string {
+	/// first we want the canonical hostname, followed by aliases, followed by any IPs
+	aliases := []string{}
+	aliases = append(aliases, s.Hostname)
+	aliases = append(aliases, s.Aliases...)
+	for _, val := range s.Ips {
+		aliases = append(aliases, val.String())
+	}
+	return aliases
 }
 
 func (s *SshPublicKey) String() string {
-  return fmt.Sprintf("FIXME")
+	return fmt.Sprintf("FIXME")
 }
 
-func RunCommand(cmdstr string) error {
-  cmd := exec.Command("sh", "-c", cmdstr)
-  log.Printf("Running: %s\n", cmdstr)
-  stdout, err := cmd.StdoutPipe()
-  if err != nil {
-    return err
-  }
-  stderr, err := cmd.StderrPipe()
-  if err != nil {
-    return err
-  }
-  if err := cmd.Start(); err != nil {
-    return err
-  }
-  se := bufio.NewScanner(stderr)
-  so := bufio.NewScanner(stdout)
-  logIO := func(s *bufio.Scanner, prefix string) {
-    for s.Scan() {
-      log.Printf("%s%s\n", prefix, s.Bytes())
-    }
-  }
-  go logIO(se, "! ")
-  go logIO(so, "> ")
-  return cmd.Wait()
+func RunCommand(cmdstr string) (string, error) {
+	output := ""
+	cmd := exec.Command("sh", "-c", cmdstr)
+	log.Printf("Running: %s\n", cmdstr)
+	combined, err := cmd.CombinedOutput()
+	// figure out the index of the null terminator
+	combined_len := bytes.Index(combined, []byte{0})
+	output = string(combined[:combined_len])
+	return output, err
 }
